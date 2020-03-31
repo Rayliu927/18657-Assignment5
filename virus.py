6850b1f02a835c20156bce8e8a0c2f6c00fcdcf1
Zirui Liu <rayliu927@gmail.com>
2020-03-30 20:04:08 -0700

    add bounce and infection

--- virus.py
+++ virus.py
@@ -6,10 +6,32 @@ import random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
- 
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 500
-BLOCK_SIZE = 25
+GREEN = (0, 255, 0)
+
+# size of screen should be 1000 and block size should be 10
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+BLOCK_SIZE = 10
+# population size = 1000
+M = 2000
+# inital infection rate = 0.1%
+X = 0.3
+# mobility = 50%
+Pm = 0.5
+# death probability = 2%
+Pd = 0.1
+# infection duration = 7 periods
+K = 100
+# S% of population stationary: 0 -> 1
+S = 0.9
+
+block_list = []
+# position:block
+block_position = dict()
+infected_block = []
+healthy_block = []
+dead_block = []
+mobile_block = []
  
  
 class Block:
@@ -21,24 +43,142 @@ class Block:
         self.y = 0
         self.change_x = 0
         self.change_y = 0
- 
- 
-def make_block():
+        # status: infected, healthy
+        self.status = None
+        # after infected, periods is set to 0
+        self.periods = -1
+        # current direction: l, r, u, d, ul, ur, dl, dr 
+        self.direction = None
+
+def make_block(status):
     """
-    Function to make a new, random ball.
+    Function to make a new block.
     """
     block = Block()
     # Starting position of the ball.
     # Take into account the ball size so we don't spawn on the edge.
     block.x = random.randrange(BLOCK_SIZE, SCREEN_WIDTH - BLOCK_SIZE)
     block.y = random.randrange(BLOCK_SIZE, SCREEN_HEIGHT - BLOCK_SIZE)
- 
-    # Speed and direction of rectangle
-    block.change_x = random.randrange(-2, 3)
-    block.change_y = random.randrange(-2, 3)
+    block.status = status
+    if status == "infected":
+        block.periods = 0
  
     return block
- 
+
+def make_block_mobile(block):
+    """
+    Function to make a block mobile.
+    """
+    directions = {(1, 1): "dr", (1, 0): "r", (1, -1): "ur", (0, 1): "d",(0, -1): "u",(-1, -1): "ul", 
+    (-1, 0): "l", (-1, 1): "dl"}
+
+    x = 0
+    y = 0
+    while x == 0 and y == 0:
+        x = random.randint(-1, 1)
+        y = random.randint(-1, 1)
+    # Speed and direction of block: -1, 0, 1
+    block.change_x = x
+    block.change_y = y
+    # block.change_x = 10
+    # block.change_y = 10
+    block.direction = directions[(x, y)]
+
+def infect(block1, block2):
+
+    if block1.status == "infected" and block2.status == "infected":
+        return
+    if block1.status == "infected":
+        block2.status = "infected"
+        block2.periods = 0
+    elif block2.status == "infected":
+        block1.status = "infected"
+        block1.periods = 0
+
+
+def mobile(block):
+    """
+    Function to make a block bounce if encounter a obstacle
+    """
+    options = [(0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    original_x, original_y = block.x, block.y
+
+    # top left corner
+    if block.x < 0 and block.y < 0:
+        options.remove((-1, 0))
+        options.remove((0, -1))
+        options.remove((-1, -1))
+    # top right corner
+    elif block.x > SCREEN_WIDTH - BLOCK_SIZE and block.y < 0:
+        options.remove((1, 0))
+        options.remove((0, -1))
+        options.remove((1, -1))
+    # botton left corner
+    elif block.x < 0 and block.y > SCREEN_HEIGHT - BLOCK_SIZE:
+        options.remove((0, -1))
+        options.remove((-1, 1))
+        options.remove((-1, 1))
+    #  botton right corner
+    elif block.x > SCREEN_WIDTH - BLOCK_SIZE and block.y > SCREEN_HEIGHT - BLOCK_SIZE:
+        options.remove((0, -1))
+        options.remove((1, 0))
+        options.remove((1, 1))
+    # top
+    elif block.y < 0:
+        options.remove((0, -1))
+    # bottom
+    elif block.y > SCREEN_HEIGHT - BLOCK_SIZE:
+        options.remove((0, 1))
+    # left 
+    elif block.x < 0:
+        options.remove((-1, 1))
+    # right
+    else:
+        options.remove((1, 0))
+
+    block.x += block.change_x
+    block.y += block.change_y
+
+    if (block.x, block.y) in block_position or block.y > SCREEN_HEIGHT - BLOCK_SIZE or block.y < 0 or block.x > SCREEN_WIDTH - BLOCK_SIZE or block.x < 0:
+        if (block.x, block.y) in block_position:
+            hit_block = block_position[(block.x, block.y)]
+            infect(block, hit_block)
+
+        while len(options) > 0:
+            block.x = original_x
+            block.y = original_y
+
+            new_direction = random.choice(options)
+            options.remove(new_direction)
+
+            block.x += new_direction[0]
+            block.y += new_direction[1]
+            if (block.x, block.y) in block_position:
+                hit_block = block_position[(block.x, block.y)]
+                infect(block, hit_block)
+
+            if (block.x, block.y) not in block_position and block.x > 0 and block.y > 0 and block.x <= SCREEN_WIDTH - BLOCK_SIZE and block.y <= SCREEN_HEIGHT - BLOCK_SIZE :
+                block.change_x = new_direction[0] 
+                block.change_y = new_direction[1]
+                break
+
+    # update the block position hashmap
+    del block_position[(original_x, original_y)]
+    block_position[(block.x, block.y)] = block
+
+    # # Bounce the block if needed
+    # if block.y > SCREEN_HEIGHT - BLOCK_SIZE or block.y < 0:
+    #     block.change_y *= -1
+    # if block.x > SCREEN_WIDTH - BLOCK_SIZE or block.x < 0:
+    #     block.change_x *= -1
+
+def die(block):
+    """
+    Function to erase a block
+    """
+    block.status = "dead"
+    block_list.remove(block)
+
  
 def main():
     """
@@ -57,11 +197,37 @@ def main():
  
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
-    block_list = []
- 
-    block = make_block()
-    block_list.append(block)
+
+    num_infected = int(M * X)
+    num_healthy = M - num_infected
+
+    # make inital infected block
+    for i in range(num_infected):
+        block = make_block("infected")
+        block_list.append(block)
+        block_position[(block.x, block.y)] = block
+
+    # make inital healthy block
+    for i in range(num_healthy):
+        block = make_block("healthy")
+        block_list.append(block)
+        block_position[(block.x, block.y)] = block
+
+    num_stationary = int(M * S)
+    num_mobile = M - num_stationary
+
+    index_block = [i for i in range(len(block_list))]
+    index_mobile_block = []
+    # randomly choose the mobile block
+    for i in range(num_mobile):
+        index = random.randint(0, len(index_block)-1)
+        index_mobile_block.append(index_block[index])
+        del index_block[index]
+
+    # make the chosen block mobile
+    for i in index_mobile_block:
+        make_block_mobile(block_list[i])
+        mobile_block.append(block_list[i])
  
     # -------- Main Program Loop -----------
     while not done:
@@ -69,35 +235,48 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            elif event.type == pygame.KEYDOWN:
-                # Space bar! Spawn a new ball.
-                if event.key == pygame.K_SPACE:
-                    block = make_block()
-                    block_list.append(block)
+            # elif event.type == pygame.KEYDOWN:
+            #     # Space bar! Spawn a new ball.
+            #     if event.key == pygame.K_SPACE:
+            #         block = make_block()
+            #         block_list.append(block)
  
         # --- Logic
-        for block in block_list:
-            # Move the ball's center
-            block.x += block.change_x
-            block.y += block.change_y
+        for block in mobile_block:
+            # Move the block's center
+            mobile(block)
+            # original_x = block.x
+            # original_y = block.y
+            # block.x += block.change_x
+            # block.y += block.change_y
  
-            # Bounce the ball if needed
-            if block.y > SCREEN_HEIGHT - BLOCK_SIZE or block.y < BLOCK_SIZE:
-                block.change_y *= -1
-            if block.x > SCREEN_WIDTH - BLOCK_SIZE or block.x < BLOCK_SIZE:
-                block.change_x *= -1
+            # change_direction(block)
  
         # --- Drawing
         # Set the screen background
-        screen.fill(BLACK)
+        screen.fill(WHITE)
  
         # Draw the balls
         for block in block_list:
-            pygame.draw.rect(screen, RED, (block.x, block.y, BLOCK_SIZE, BLOCK_SIZE))
+            if block.status == "infected":
+                block.periods += 1
+                if block.periods == K:
+                    # the block is either dead or cured
+                    poss = random.uniform(0, 1)
+                    if poss < Pd:
+                        die(block)
+                        continue
+                    else:
+                        block.status = "cured"
+                pygame.draw.rect(screen, RED, (block.x, block.y, BLOCK_SIZE, BLOCK_SIZE))
+            elif block.status == "cured":
+                pygame.draw.rect(screen, GREEN, (block.x, block.y, BLOCK_SIZE, BLOCK_SIZE))
+            else:
+                pygame.draw.rect(screen, BLACK, (block.x, block.y, BLOCK_SIZE, BLOCK_SIZE))
  
         # --- Wrap-up
         # Limit to 60 frames per second
-        clock.tick(60)
+        clock.tick(10)
  
         # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
